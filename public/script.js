c7e9055f27817a9061e25ebd005669350b1e265c
Fuann-Kinoko <838832306@qq.com>
2023-11-27 18:48:32 +0800

    make it clean

--- public/script.js
+++ public/script.js
@@ -6,40 +6,68 @@ let ctx = canvas.getContext("2d");
 
 var io = io().connect('http://localhost:8080')
 
-let pencilButton = document.getElementById('pencilButton');
-let rectangleButton = document.getElementById('rectangleButton');
-let circleButton = document.getElementById('circleButton');
-let textButton = document.getElementById('textButton');
-let resetButton = document.getElementById('resetButton');
-let eraserButton = document.getElementById('eraserButton');
+
+/*
+====================
+init webpage buttons
+====================
+*/
+const getButton = btt => document.getElementById(btt);
+
+let pencilButton = getButton('pencilButton');
+let rectangleButton = getButton('rectangleButton');
+let circleButton = getButton('circleButton');
+let textButton = getButton('textButton');
+let resetButton = getButton('resetButton');
+let eraserButton = getButton('eraserButton');
+
+
+/*
+===========
+init canvas
+===========
+*/
 let drawMode = 'pencil';
 let eraserMode = false;
+let x, y;
+let pressed = false;
+let width, height;
+let radius;
+const font = '14px sans-serif';
+let rect = canvas.getBoundingClientRect();
 
-pencilButton.addEventListener('click', function() {
-    drawMode = 'pencil';
-    console.log("pencil");
-});
-
-rectangleButton.addEventListener('click', function() {
-    drawMode = 'rectangle';
-    console.log("rectangle");
-});
-
-circleButton.addEventListener('click', function() {
-    drawMode = 'circle';
-    console.log("circle");
-});
-
-textButton.addEventListener('click', function() {
-    drawMode = 'text';
-    console.log("text");
-});
-
-eraserButton.addEventListener('click', function() {
-    drawMode = 'eraser';
-    console.log("eraser");
-
-});
+/*
+===========================
+addEventListner for buttons
+===========================
+*/
+function addDrawModeChangeEvent(btt, method) {
+    btt.addEventListener('click', function() {
+        method;
+    });
+}
+function addRegularDrawmodeChangeEvent(btt, bttMode) {
+    addDrawModeChangeEvent(
+        btt,
+        function() {
+            drawMode = bttMode;
+            console.log(bttMode);
+        }
+    );
+}
+addRegularDrawmodeChangeEvent(pencilButton, 'pencil');
+addRegularDrawmodeChangeEvent(rectangleButton, 'rectangle');
+addRegularDrawmodeChangeEvent(circleButton, 'circle');
+addRegularDrawmodeChangeEvent(textButton, 'text');
+addRegularDrawmodeChangeEvent(eraserButton, 'eraser');
+// addDrawModeChangeEvent(
+//     resetButton,
+//     function() {
+//         drawMode = 'reset';
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
+//         console.log("reset");
+//     }
+// );
 
 // io.on('onreset', () => {
 //     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -51,65 +79,104 @@ eraserButton.addEventListener('click', function() {
 //     io.emit('reset', {})
 // });
 
-let x;
-let y;
-let pressed = false;
-let width;
-let height;
-let radius;
-const font = '14px sans-serif';
-let rect = canvas.getBoundingClientRect();
 
-// // 直线
-// io.on('ondown', ({x, y}) => {
-//     ctx.moveTo(x, y);
-// })
 
-// window.onmousedown = (e) => {
-//     x = e.clientX;
-//     y = e.clientY;
-//     ctx.moveTo(x, y);
-//     io.emit('down', {x, y});
-//     pressed = true;
-// }
+/*
+=========================
+add handlers for io event
+=========================
+*/
+io.on('ondown', ({x, y}) => { ctx.moveTo(x, y); })
+io.on('ondrawLine', drawLine(x, y))
+io.on('ondrawRect', drawRectangle(x, y, width, height))
+io.on('ondrawCirc', drawCircle(centerX, centerY, radius))
+io.on('onwriteText', writeText(txt, x, y))
+io.on('oneraser', erase(x,y))
 
-// window.onmouseup = (e) => {
-//     pressed = false;
-// }
 
-// // 让别人看到
-// io.on('ondraw', ({x, y}) => {
-//     ctx.lineTo(x, y);
-//     ctx.stroke();
-// })
-
-// window.onmousemove = (e) => {
-//     x = e.clientX;
-//     y = e.clientY;
-//     console.log({x, y});
-//     if (pressed == true) {
-//         io.emit('draw', {x, y})
-//         // 让自己看到
-//         ctx.lineTo(x, y);
-//         ctx.stroke();
-//     }
-// };
-
+function drawLine(x, y) {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+}
 function drawRectangle(x, y, width, height) {
     ctx.beginPath();
     ctx.rect(x, y, width, height);
     ctx.stroke();
 }
-
 function drawCircle(centerX, centerY, radius) {
     ctx.beginPath();
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
     ctx.stroke();
 }
+function writeText(txt, x, y) {
+    ctx.fillText(txt, x - 4, y - 4);
+}
+function erase(x, y) {
+    ctx.globalCompositeOperation = 'destination-out'; // 设置混合模式为destination-out，即删除模式
+    ctx.beginPath();
+    ctx.arc(x, y, 30, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.globalCompositeOperation = 'source-over'; // 恢复混合模式
+}
 
+/*
+=======================
+add mouse event emitter
+=======================
+*/
+window.onmousedown = (e) => {
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    ctx.moveTo(x, y);
+    io.emit('down', {x, y});
+    pressed = true;
+    switch (drawMode) {
+        case 'eraser':
+            eraserMode = true;
+            erase(x,y);
+            io.emit('eraser', {x, y});
+    }
+}
+window.onmouseup = (e) => {
+    pressed = false;
+    eraserMode = false;
+    switch (drawMode) {
+        case 'rectangle':
+            width = e.clientX - rect.left - x;
+            height = e.clientY - rect.top - y;
+            drawRectangle(x, y, width, height);
+            io.emit('drawRect', {x, y, width, height});
+        case 'circle':
+            var centerX = (e.clientX - rect.left + x) / 2;
+            var centerY = (e.clientY - rect.top + y) / 2;
+            radius = Math.sqrt(Math.pow(e.clientX - rect.left - centerX, 2) + Math.pow(e.clientY - rect.top - centerY, 2));
+            drawCircle(centerX, centerY, radius);
+            io.emit('drawCirc', {centerX, centerY, radius});
+    }
+}
+window.onmousemove = (e) => {
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
+    switch (drawMode) {
+        case 'pencil':
+            if (pressed != true) return;
+            drawLine(x, y);
+            io.emit('drawLine', {x, y})
+        case 'eraser':
+            if (eraserMode != true) return;
+            erase(x,y);
+            io.emit('eraser', {x, y});
+    }
+};
+
+/*
+===============================================
+handle tool that requires mouseClick: text tool
+===============================================
+*/
 const textTool = {
     hasInput: false,
-    
+
     addInput(x, y) {
         var input = document.createElement('input');
 
@@ -117,13 +184,13 @@ const textTool = {
         input.style.position = 'fixed';
         input.style.left = (x - 4) + 'px';
         input.style.top = (y - 4) + 'px';
-    
+
         input.onkeydown = textTool.handleEnter;
-    
+
         document.body.appendChild(input);
-    
+
         input.focus();
-    
+
         hasInput = true;
     },
 
@@ -144,95 +211,6 @@ const textTool = {
         io.emit('writeText', {txt, x, y})
     },
 };
-
-
-io.on('ondown', ({x, y}) => {
-    ctx.moveTo(x, y);
-})
-io.on('oneraser', ({x, y}) => {
-    ctx.globalCompositeOperation = 'destination-out'; // 设置混合模式为destination-out，即删除模式
-    ctx.beginPath();
-    ctx.arc(x, y, 30, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.globalCompositeOperation = 'source-over'; // 恢复混合模式
-})
-window.onmousedown = (e) => {
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
-    ctx.moveTo(x, y);
-    io.emit('down', {x, y});
-    pressed = true;
-
-    if (drawMode === 'eraser') {
-        eraserMode = true;
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        ctx.globalCompositeOperation = 'destination-out'; // 设置混合模式为destination-out，即删除模式
-        ctx.beginPath();
-        ctx.arc(x, y, 30, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.globalCompositeOperation = 'source-over'; // 恢复混合模式
-        io.emit('eraser', {x, y});
-    }
-}
-
-io.on('ondrawRect', ({x, y, width, height}) => {
-    drawRectangle(x, y, width, height);
-})
-io.on('ondrawCirc', ({centerX, centerY, radius}) => {
-    drawCircle(centerX, centerY, radius);
-})
-
-window.onmouseup = (e) => {
-    pressed = false;
-    eraserMode = false;
-    if (drawMode == 'rectangle') {
-        width = e.clientX - rect.left - x;
-        height = e.clientY - rect.top - y;
-        drawRectangle(x, y, width, height);
-        io.emit('drawRect', {x, y, width, height});
-    }
-    if (drawMode == 'circle') {
-        var centerX = (e.clientX - rect.left + x) / 2;
-        var centerY = (e.clientY - rect.top + y) / 2;
-        radius = Math.sqrt(Math.pow(e.clientX - rect.left - centerX, 2) + Math.pow(e.clientY - rect.top - centerY, 2));
-        drawCircle(centerX, centerY, radius);
-        io.emit('drawCirc', {centerX, centerY, radius});
-    }
-}
-
-io.on('ondrawLine', ({x, y}) => {
-    ctx.lineTo(x, y);
-    ctx.stroke();
-})
-
-window.onmousemove = (e) => {
-    if (drawMode == 'pencil') {    
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        // console.log({x, y});
-        if (pressed == true) {
-            io.emit('drawLine', {x, y})
-            ctx.lineTo(x, y);
-            ctx.stroke();
-        }
-    }
-
-    if (drawMode === 'eraser' && eraserMode) {
-        x = e.clientX - rect.left;
-        y = e.clientY - rect.top;
-        ctx.globalCompositeOperation = 'destination-out';
-        ctx.beginPath();
-        ctx.arc(x, y, 30, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.globalCompositeOperation = 'source-over';
-        io.emit('eraser', {x, y});
-    }
-};
-
-io.on('onwriteText', ({txt, x, y}) => {
-    ctx.fillText(txt, x - 4, y - 4);
-})
 
 // 文本框
 canvas.onclick = function(e) {
