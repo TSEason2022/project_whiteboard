23a8de36094d80268b81649682cc9375638a5e46
n19htfall <ziheng_liu@outlook.com>
2023-11-28 01:41:52 +0800

    add rectangle preview animation

--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
 let canvas = document.getElementById('board');
+let animation = document.getElementById('animation');
 
 canvas.width = 0.9 * window.innerWidth;
 canvas.height = 0.9 * window.innerHeight;
 let ctx = canvas.getContext("2d");
+
+animation.width = 0.9 * window.innerWidth;
+animation.height = 0.9 * window.innerHeight;
+let ctx2 = animation.getContext("2d");
 
 var io = io().connect('http://localhost:8080')
 
@@ -54,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function addDrawModeChangeEvent(btt, method) {
-    btt.addEventListener('click', method );
+    btt.addEventListener('click', method);
 }
 function addRegularDrawmodeChangeEvent(btt, bttMode) {
     addDrawModeChangeEvent(
@@ -87,12 +92,12 @@ addDrawModeChangeEvent(
 add handlers for io event
 =========================
 */
-io.on('ondown', ({x, y}) => { ctx.moveTo(x, y); })
-io.on('ondrawLine', ({x,y}) => drawLine(x, y))
-io.on('ondrawRect', ({x, y, width, height}) => drawRectangle(x, y, width, height))
-io.on('ondrawCirc', ({centerX, centerY, radius}) => drawCircle(centerX, centerY, radius))
-io.on('onwriteText', ({txt,x,y}) => writeText(txt, x, y))
-io.on('oneraser', ({x,y}) => erase(x,y))
+io.on('ondown', ({ x, y }) => { ctx.moveTo(x, y); })
+io.on('ondrawLine', ({ x, y }) => drawLine(x, y))
+io.on('ondrawRect', ({ x, y, width, height }) => drawRectangle(x, y, width, height))
+io.on('ondrawCirc', ({ centerX, centerY, radius }) => drawCircle(centerX, centerY, radius))
+io.on('onwriteText', ({ txt, x, y }) => writeText(txt, x, y))
+io.on('oneraser', ({ x, y }) => erase(x, y))
 io.on('onreset', () => reset())
 
 
@@ -100,19 +105,40 @@ function drawLine(x, y) {
     ctx.lineTo(x, y);
     ctx.stroke();
 }
+
+function rectangleAnimation(x, y, width, height) {
+    ctx2.beginPath();
+    ctx2.lineWidth = 2;
+    ctx2.rect(x, y, width, height);
+    ctx2.clearRect(0, 0, canvas.width, canvas.height);
+    ctx2.stroke();
+}
+
+function circleAnimation(centerX, centerY, radius) {
+    ctx2.beginPath();
+    ctx2.lineWidth = 2;
+    ctx2.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx2.stroke();
+}
+
 function drawRectangle(x, y, width, height) {
     ctx.beginPath();
+    ctx.lineWidth = 2;
     ctx.rect(x, y, width, height);
     ctx.stroke();
 }
+
 function drawCircle(centerX, centerY, radius) {
     ctx.beginPath();
+    ctx2.lineWidth = 2;
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
     ctx.stroke();
 }
+
 function writeText(txt, x, y) {
     ctx.fillText(txt, x - 4, y - 4);
 }
+
 function erase(x, y) {
     ctx.globalCompositeOperation = 'destination-out'; // 设置混合模式为destination-out，即删除模式
     ctx.beginPath();
@@ -120,8 +146,10 @@ function erase(x, y) {
     ctx.fill();
     ctx.globalCompositeOperation = 'source-over'; // 恢复混合模式
 }
+
 function reset() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx2.clearRect(0, 0, animation.width, animation.height);
     ctx.beginPath();  // clear previous path
 }
 
@@ -137,12 +165,12 @@ window.onmousedown = (e) => {
     io.emit('down', { x, y });
     pressed = true;
 
-    console.log('pressing down', 'for', {x, y});
+    console.log('pressing down', 'for', { x, y });
     switch (drawMode) {
         case 'eraser':
             eraserMode = true;
-            erase(x,y);
-            io.emit('eraser', {x, y});
+            erase(x, y);
+            io.emit('eraser', { x, y });
             break;
     }
 }
@@ -154,14 +182,16 @@ window.onmouseup = (e) => {
             width = e.clientX - rect.left - x;
             height = e.clientY - rect.top - y;
             drawRectangle(x, y, width, height);
-            io.emit('drawRect', {x, y, width, height});
+            ctx2.clearRect(0, 0, animation.width, animation.height);
+            io.emit('drawRect', { x, y, width, height });
             break;
         case 'circle':
             var centerX = (e.clientX - rect.left + x) / 2;
             var centerY = (e.clientY - rect.top + y) / 2;
             radius = Math.sqrt(Math.pow(e.clientX - rect.left - centerX, 2) + Math.pow(e.clientY - rect.top - centerY, 2));
             drawCircle(centerX, centerY, radius);
-            io.emit('drawCirc', {centerX, centerY, radius});
+            ctx2.clearRect(0, 0, animation.width, animation.height);
+            io.emit('drawCirc', { centerX, centerY, radius });
             break;
     }
 }
@@ -172,14 +202,31 @@ window.onmousemove = (e) => {
             y = e.clientY - rect.top;
             if (pressed != true) break;
             drawLine(x, y);
-            io.emit('drawLine', {x, y})
+            io.emit('drawLine', { x, y })
             break;
         case 'eraser':
             x = e.clientX - rect.left;
             y = e.clientY - rect.top;
             if (eraserMode != true) break;
-            erase(x,y);
-            io.emit('eraser', {x, y});
+            erase(x, y);
+            io.emit('eraser', { x, y });
+            break;
+        case 'rectangle':
+            if (pressed) {
+                width = e.clientX - rect.left - x;
+                height = e.clientY - rect.top - y;
+                rectangleAnimation(x, y, width, height);
+                io.emit('drawRect', { x, y, width, height });
+            }
+            break;
+        case 'circle':
+            if (pressed) {
+                var centerX = (e.clientX - rect.left + x) / 2;
+                var centerY = (e.clientY - rect.top + y) / 2;
+                radius = Math.sqrt(Math.pow(e.clientX - rect.left - centerX, 2) + Math.pow(e.clientY - rect.top - centerY, 2));
+                circleAnimation(centerX, centerY, radius);
+            }
+            io.emit('drawCirc', { centerX, centerY, radius });
             break;
     }
 };
